d59194a39be5b118c7bfa954de15f1a0b4f7fd38
Sylvain ANTON <121446480+sylvainatn@users.noreply.github.com>
2025-07-12 15:42:34 +0200

    Delete .history

--- .history/themes/ESGI-theme/page-blog_20250711011742.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-
-<main class="container">
-    <?php get_template_part('template-parts/hero-section'); ?>
-
-</main>
-
-<?php get_footer(); ?>
--- .history/themes/ESGI-theme/page-blog_20250711012859.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-
-<main class="container">
-      <h1 class="hero-title">Blog</h1>
-   </div>
-</main>
-
-<?php get_footer(); ?>

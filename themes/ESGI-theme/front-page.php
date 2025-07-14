607e395c06e6bc8e46781c47cec116b9de8e2f08
shany10 <shanyahsam@gmail.com>
2025-07-14 18:46:54 +0200

    add responsive home page

--- themes/ESGI-theme/front-page.php
+++ themes/ESGI-theme/front-page.php
@@ -2,19 +2,19 @@
 
 <main class="container">
         <div id="heros">
-            <?php //get_template_part('template-parts/hero-section'); ?>
+            <?php get_template_part('template-parts/hero-section'); ?>
         </div>
 
         <div id="about">
-            <?php //get_template_part('template-parts/about-section'); ?>
+            <?php get_template_part('template-parts/about-section'); ?>
         </div>
 
         <div id="services">
-            <?php //get_template_part('template-parts/services-section'); ?>
+            <?php get_template_part('template-parts/services-section'); ?>
         </div>
 
         <div id="partners">
-            <?php //get_template_part('template-parts/partners-section'); ?>
+            <?php get_template_part('template-parts/partners-section'); ?>
         </div>
 </main>
 
